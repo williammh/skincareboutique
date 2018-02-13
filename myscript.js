d547937f4feed4e7122956f516c1cc1471f1ea43
unknown <huynh823@email.arizona.edu>
2018-02-12 18:38:08 -0800

    first commit

---
+++ myscript.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $("#splash").css({
+            "opacity" : 0,
+        });
+        $("#main-container").css({
+            "opacity" : 1,
+        });
+        $("#topbar").animate({
+            "top" : "0px",
+        });
+    });
+
+    $("#main-container").html($("#about-content").html())
+
+    $("#navabout").click(function(){
+        var content = $("#about-content").html()
+        $("#main-container").html(content)
+    });
+    $("#navservices").click(function(){
+        var content = $("#services-content").html()
+        $("#main-container").html(content)
+    });
+    $("#navlocation").click(function(){
+        var content = $("#location-content").html()
+        $("#main-container").html(content)
+    });
+    $("#navcontact").click(function(){
+        var content = $("#contact-content").html()
+        $("#main-container").html(content)
+    });
+    $("#navphotos").click(function(){
+        var content = $("#photos-content").html()
+        $("#main-container").html(content)
+    });
+})
+// google maps api key AIzaSyAnjD9kV9KM-ft9pPPchNKQBW427-TlUUw
