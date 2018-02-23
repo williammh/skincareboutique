a6d91e8e35cae174f50048ef6efb10c2e03973f9
unknown <huynh823@email.arizona.edu>
2018-02-23 06:40:03 -0800

    fixed6

--- myscript.js
+++ myscript.js
@@ -19,8 +19,8 @@ $(document).ready(function(){
     });
     $("#navservices").click(function(){
         var content = $("#services-content").html()
-        setFontSize();
         $("#main-container").html(content);
+        setFontSize();
         $("#main-container").css("height", "auto");
     });
     $("#navlocation").click(function(){
@@ -41,7 +41,6 @@ $(document).ready(function(){
     // to force css rules if a mobile device is in portrait mode AND still has a window innerWidth > 672px
     function setFontSize(){
         if(window.innerWidth < window.innerHeight){
-            $("#main-container").css("height", window.innerHeight);
             $("h4").css({
                 "font-size" : "1.75rem"
             });
