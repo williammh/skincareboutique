36dccb10233fd434682da22428aee40e228ca61e
unknown <huynh823@email.arizona.edu>
2018-02-23 07:27:16 -0800

    fix11

--- myscript.js
+++ myscript.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
     $("#navservices").click(function(){
         var content = $("#services-content").html();
         $("#main-container").html(content);
-        $("#main-container").css("height", "auto");
+        setFontSize();
+        $("#main-container").css("height", $(document).height());
     });
     $("#navlocation").click(function(){
         var content = $("#location-content").html()
