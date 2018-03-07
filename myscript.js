4a31739d070cce90242d378588caa0dc3bcafa02
unknown <huynh823@email.arizona.edu>
2018-03-07 07:25:49 -0800

    top bar alignment, google map size

--- myscript.js
+++ myscript.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
         // see https://www.thesitewizard.com/archive/framebreak.shtml
         // for an explanation of this script and how to use it on your
         // own website
+
         if (top.location != location) {
             top.location.href = document.location.href ;
         }
